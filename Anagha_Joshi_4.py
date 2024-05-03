f8fddf994aceea82782f36fda2078f0c2236afce
Anagha <anaghabj53@yahoo.com>
2024-05-03 10:31:00 -0400

    Commit for Assignment-4 of course 2

---
+++ Anagha_Joshi_4.py
@@ -0,0 +1,38 @@
+# Task 2
+
+firstName= "Anagha"
+middleName= "Vishnu"
+lastName= "Baxi"
+print("My Fullname is {} {} {}".format(firstName, middleName, lastName))
+print(f"My Fullname is {firstName} {middleName} {lastName}")
+
+#Task 3
+
+a=5
+print(f"the value of a is {a} and the datatype is {type(a)}")
+
+b=7.5
+print(f"the value of b is {b} and the datatype is {type(b)}")
+
+c="abcd"
+print(f"the value of c is {c} and the datatype is {type(c)}")
+
+d=3+4j
+print(f"the value of d is {d} and the datatype is {type(d)}")
+
+e=True
+print(f"the value of e is {e} and the datatype is {type(e)}")
+
+f=["John", "James", "Jammie"]
+print(f"the value of f is {f} and the datatype is {type(f)}")
+
+g=range(1,21,3)
+for n in g:
+    print(f"the value of g is {n}")
+print(f"the datatype of g is {type(g)}")
+
+
+
+
+
+
